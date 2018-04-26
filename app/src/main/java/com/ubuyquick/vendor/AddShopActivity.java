22a28830a3dd990fca0a70a2ad93d2fd920bfe93
Ajay Srinivas <ajay.s1997@gmail.com>
2018-04-26 22:01:54 +0530

    Complete ShopLocationActivity for searching and setting shop location and radius.

--- app/src/main/java/com/ubuyquick/vendor/AddShopActivity.java
+++ app/src/main/java/com/ubuyquick/vendor/AddShopActivity.java
@@ -1,10 +1,32 @@
 package com.ubuyquick.vendor;
 
+import android.Manifest;
+import android.app.TimePickerDialog;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.TimePicker;
+import android.widget.Toast;
 
 public class AddShopActivity extends AppCompatActivity {
+
+    private static final String TAG = "AddShopActivity";
+
+    private Button btn_from, btn_to, btn_location, btn_add_shop;
+    private CheckBox cb_groceries, cb_provisions, cb_livestock, cb_vegetables;
+
+    private String timings_from = null, timings_to = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,16 +35,101 @@ public class AddShopActivity extends AppCompatActivity {
         this.getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_action_up);
         this.getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        initialize();
         initializeViews();
+        initialize();
     }
 
     private void initialize() {
 
+        btn_location.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    if (ActivityCompat.checkSelfPermission(AddShopActivity.this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(AddShopActivity.this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                        requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, 101);
+                    } else {
+                        startActivity(new Intent(AddShopActivity.this, ShopLocationActivity.class));
+                    }
+                }
+            }
+        });
+
+        btn_from.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TimePickerDialog timePickerDialog = new TimePickerDialog(AddShopActivity.this,
+                        new TimePickerDialog.OnTimeSetListener() {
+                            @Override
+                            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                                if (hourOfDay < 12) {
+                                    timings_from = String.format("%02d", hourOfDay) + ":" + String.format("%02d", minute) + " AM";
+                                    btn_from.setText(timings_from);
+                                } else if (hourOfDay > 12) {
+                                    timings_from = String.format("%02d", hourOfDay % 12) + ":" + String.format("%02d", minute) + " PM";
+                                    btn_from.setText(timings_from);
+                                } else {
+                                    timings_from = String.format("%02d", hourOfDay) + ":" + String.format("%02d", minute) + " PM";
+                                    btn_from.setText(timings_from);
+                                }
+                            }
+                        }, 0, 0, false);
+                timePickerDialog.show();
+
+            }
+        });
+
+        btn_to.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TimePickerDialog timePickerDialog = new TimePickerDialog(AddShopActivity.this,
+                        new TimePickerDialog.OnTimeSetListener() {
+                            @Override
+                            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                                if (hourOfDay < 12) {
+                                    timings_to = String.format("%02d", hourOfDay) + ":" + String.format("%02d", minute) + " AM";
+                                    btn_to.setText(timings_to);
+                                } else if (hourOfDay > 12) {
+                                    timings_to = String.format("%02d", hourOfDay % 12) + ":" + String.format("%02d", minute) + " PM";
+                                    btn_to.setText(timings_to);
+                                } else {
+                                    timings_to = String.format("%02d", hourOfDay) + ":" + String.format("%02d", minute) + " PM";
+                                    btn_to.setText(timings_to);
+                                }
+                            }
+                        }, 0, 0, false);
+                timePickerDialog.show();
+
+            }
+        });
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch(requestCode) {
+            case 101:
+                if (grantResults.length <= 0 || grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(this, "Location required to set radius", Toast.LENGTH_SHORT).show();
+                } else {
+                    startActivity(new Intent(AddShopActivity.this, ShopLocationActivity.class));
+                }
+                break;
+
+                default:
+                    super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
     }
 
     private void initializeViews() {
+        btn_from = (Button) findViewById(R.id.btn_from);
+        btn_to = (Button) findViewById(R.id.btn_to);
+        btn_location = (Button) findViewById(R.id.btn_location);
+        btn_add_shop = (Button) findViewById(R.id.btn_add_shop);
 
+        cb_groceries = (CheckBox) findViewById(R.id.checkBox2);
+        cb_vegetables = (CheckBox) findViewById(R.id.checkBox3);
+        cb_livestock = (CheckBox) findViewById(R.id.checkBox4);
+        cb_provisions = (CheckBox) findViewById(R.id.checkBox5);
     }
 
     @Override
---
+++ app/src/main/java/com/ubuyquick/vendor/ShopLocationActivity.java
@@ -0,0 +1,218 @@
+package com.ubuyquick.vendor;
+
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationManager;
+import android.provider.Settings;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CircleOptions;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ShopLocationActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    private static final String TAG = "ShopLocationActivity";
+
+    private final float DEFAULT_ZOOM = 15.0f;
+    private double delivery_radius = 100.0D;
+
+    private GoogleMap shopLocationMap;
+    private EditText et_delivery_radius, et_search;
+    private Button btn_set_radius, btn_set_location, btn_search;
+
+    private double lat = 12.9716, lng = 77.5946;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shop_location);
+
+//        this.getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_action_up);
+//        this.getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        et_delivery_radius = (EditText) findViewById(R.id.et_delivery_radius);
+        et_search = (EditText) findViewById(R.id.et_search);
+        btn_set_radius = (Button) findViewById(R.id.btn_set_radius);
+        btn_search = (Button) findViewById(R.id.btn_search);
+        btn_set_location = (Button) findViewById(R.id.btn_set_location);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+        btn_set_radius.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                shopLocationMap.clear();
+                MarkerOptions markerOptions = new MarkerOptions()
+                        .position(new LatLng(lat, lng))
+                        .draggable(true)
+                        .title(lat + "," + lng);
+                shopLocationMap.addMarker(markerOptions);
+                shopLocationMap.addCircle(new CircleOptions()
+                        .center(new LatLng(lat, lng)).radius(Double.parseDouble(et_delivery_radius.getText().toString())));
+            }
+        });
+
+        btn_search.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "onClick: geolocating");
+                Geocoder geocoder = new Geocoder(ShopLocationActivity.this);
+                List<Address> list = new ArrayList<>();
+                try {
+                    list = geocoder.getFromLocationName(et_search.getText().toString(), 1);
+                } catch (IOException e) {
+                    Log.d(TAG, "onClick: " + e.getLocalizedMessage());
+                }
+
+                if (list.size() > 0) {
+                    Address address = list.get(0);
+                    Log.d(TAG, "onClick: location found; " + address.toString());
+                    shopLocationMap.clear();
+                    lat = address.getLatitude();
+                    lng = address.getLongitude();
+                    LatLng latLng = new LatLng(lat, lng);
+                    MarkerOptions markerOptions = new MarkerOptions()
+                            .position(latLng)
+                            .draggable(true)
+                            .title(address.getLocality());
+
+                    if (!TextUtils.isEmpty(et_delivery_radius.getText())) {
+                        delivery_radius = Double.parseDouble(et_delivery_radius.getText().toString());
+                    }
+
+                    shopLocationMap.addMarker(markerOptions);
+                    shopLocationMap.addCircle(new CircleOptions().radius(delivery_radius)
+                            .center(latLng));
+                    shopLocationMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, DEFAULT_ZOOM));
+
+                }
+            }
+        });
+    }
+
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        shopLocationMap = googleMap;
+        LatLng india = new LatLng(lat, lng);
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        LocationManager lm = (LocationManager) getApplicationContext().getSystemService(Context.LOCATION_SERVICE);
+        if (!lm.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            AlertDialog.Builder dialog = new AlertDialog.Builder(ShopLocationActivity.this);
+            dialog.setMessage("Please enable GPS to use My Location");
+            dialog.setPositiveButton("Settings", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface paramDialogInterface, int paramInt) {
+                    Intent myIntent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                    ShopLocationActivity.this.startActivity(myIntent);
+                }
+            });
+            dialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+
+                @Override
+                public void onClick(DialogInterface paramDialogInterface, int paramInt) {
+
+                }
+            });
+            dialog.show();
+        }
+
+        shopLocationMap.setMyLocationEnabled(true);
+        shopLocationMap.moveCamera(CameraUpdateFactory.newLatLngZoom(india, 15.0f));
+
+        shopLocationMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+            @Override
+            public void onMapClick(LatLng latLng) {
+                lat = latLng.latitude;
+                lng = latLng.longitude;
+                MarkerOptions markerOptions = new MarkerOptions()
+                        .position(latLng)
+                        .draggable(true)
+                        .title(lat + "," + lng);
+
+                if (!TextUtils.isEmpty(et_delivery_radius.getText())) {
+                    delivery_radius = Double.parseDouble(et_delivery_radius.getText().toString());
+                }
+
+                shopLocationMap.clear();
+                shopLocationMap.addMarker(markerOptions);
+                shopLocationMap.addCircle(new CircleOptions().radius(delivery_radius)
+                        .center(latLng));
+            }
+        });
+
+        shopLocationMap.setOnMarkerDragListener(new GoogleMap.OnMarkerDragListener() {
+            @Override
+            public void onMarkerDragStart(Marker marker) {
+
+            }
+
+            @Override
+            public void onMarkerDrag(Marker marker) {
+
+            }
+
+            @Override
+            public void onMarkerDragEnd(Marker marker) {
+                LatLng latLng = marker.getPosition();
+                lat = latLng.latitude;
+                lng = latLng.longitude;
+                MarkerOptions markerOptions = new MarkerOptions()
+                        .position(latLng)
+                        .draggable(true)
+                        .title(lat + "," + lng);
+
+                if (!TextUtils.isEmpty(et_delivery_radius.getText())) {
+                    delivery_radius = Double.parseDouble(et_delivery_radius.getText().toString());
+                }
+
+                shopLocationMap.clear();
+                shopLocationMap.addMarker(markerOptions);
+                shopLocationMap.addCircle(new CircleOptions().radius(delivery_radius)
+                        .center(latLng));
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if (item.getItemId() == android.R.id.home) {
+            onBackPressed();
+        }
+        return false;
+    }
+}
