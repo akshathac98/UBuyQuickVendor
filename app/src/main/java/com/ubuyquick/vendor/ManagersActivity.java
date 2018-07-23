f42e35c4b4cf5dc80e7c8968271cc8f7981bf35a
Ajay Srinivas <ajay.s1997@gmail.com>
2018-07-23 21:45:13 +0530

    Complete adding new product.

--- app/src/main/java/com/ubuyquick/vendor/ManagersActivity.java
+++ app/src/main/java/com/ubuyquick/vendor/ManagersActivity.java
@@ -7,8 +7,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.InputType;
 import android.util.Log;
+import android.view.ContextMenu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
@@ -39,6 +42,7 @@ public class ManagersActivity extends AppCompatActivity {
     private String shop_id, vendor_number, shop_name;
     private List<String> managers;
     private RelativeLayout btn_add_manager;
+    private ArrayAdapter<String> arrayAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -58,8 +62,15 @@ public class ManagersActivity extends AppCompatActivity {
 
         list_managers = (ListView) findViewById(R.id.lv_managers);
         managers = new ArrayList<String>();
-        final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, managers);
+        arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, managers);
         list_managers.setAdapter(arrayAdapter);
+        registerForContextMenu(list_managers);
+        list_managers.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                return false;
+            }
+        });
 
         db.collection("vendors").document(mAuth.getCurrentUser().getPhoneNumber().substring(3))
                 .collection("shops").document(shop_id).collection("managers").get()
@@ -140,6 +151,107 @@ public class ManagersActivity extends AppCompatActivity {
                         });
                     }
                 });
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        if (v.getId() == R.id.lv_managers) {
+            MenuInflater inflater = getMenuInflater();
+            inflater.inflate(R.menu.assign_menu, menu);
+        }
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        final AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
+        switch (item.getItemId()) {
+            case R.id.action_edit:
+                AlertDialog.Builder builder = new AlertDialog.Builder(ManagersActivity.this);
+                builder.setTitle("Manager mobile number:");
+                input = new EditText(ManagersActivity.this);
+                input.setText(managers.get(info.position));
+                input.setInputType(InputType.TYPE_CLASS_PHONE);
+                builder.setView(input);
+                builder.setNegativeButton("Cancel", null);
+
+                builder.setPositiveButton("Save", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        final Map<String, Object> agent = new HashMap<>();
+                        agent.put("user_id", input.getText().toString());
+                        agent.put("user_role", "MANAGER");
+
+                        db.collection("managers").document(managers.get(info.position)).delete();
+                        db.collection("vendors").document(mAuth.getCurrentUser().getPhoneNumber().substring(3))
+                                .collection("shops").document(shop_id).collection("managers")
+                                .document(managers.get(info.position)).delete();
+
+                        db.collection("managers").document(input.getText().toString()).get()
+                                .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                                        if (task.getResult().exists()) {
+                                            Map<String, Object> shop = new HashMap<>();
+                                            shop.put("shop_name", shop_name);
+                                            shop.put("vendor_id", vendor_number);
+                                            shop.put("shop_id", shop_id);
+
+                                            db.collection("managers").document(input.getText().toString()).collection("shops").document(shop_id)
+                                                    .set(shop);
+                                            db.collection("vendors").document(mAuth.getCurrentUser().getPhoneNumber().substring(3))
+                                                    .collection("shops").document(shop_id).collection("managers")
+                                                    .document(input.getText().toString()).set(agent);
+                                        } else {
+                                            db.collection("managers").document(input.getText().toString()).set(agent)
+                                                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                                        @Override
+                                                        public void onComplete(@NonNull Task<Void> task) {
+                                                            Map<String, Object> shop = new HashMap<>();
+                                                            shop.put("shop_name", shop_name);
+                                                            shop.put("vendor_id", vendor_number);
+                                                            shop.put("shop_id", shop_id);
+                                                            db.collection("managers").document(input.getText().toString()).collection("shops").document(shop_id)
+                                                                    .set(shop);
+                                                        }
+                                                    });
+                                        }
+                                        managers.set(info.position, input.getText().toString());
+                                        arrayAdapter.notifyDataSetChanged();
+                                    }
+                                });
+                    }
+                });
+
+                builder.show();
+                return super.onContextItemSelected(item);
+
+
+            case R.id.action_delete:
+                AlertDialog.Builder builder2 = new AlertDialog.Builder(ManagersActivity.this);
+                builder2.setTitle("Delete manager?");
+                builder2.setNegativeButton("Cancel", null);
+
+                builder2.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        db.collection("managers").document(managers.get(info.position)).delete();
+                        db.collection("vendors").document(mAuth.getCurrentUser().getPhoneNumber().substring(3))
+                                .collection("shops").document(shop_id).collection("managers")
+                                .document(managers.get(info.position)).delete();
+                        managers.remove(info.position);
+                        arrayAdapter.notifyDataSetChanged();
+                    }
+                });
+
+                builder2.show();
+                return super.onContextItemSelected(item);
+
+
+            default:
+                return super.onContextItemSelected(item);
+
+        }
     }
 
     @Override
